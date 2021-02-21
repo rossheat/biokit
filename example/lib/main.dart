087b101d158e474c948af47b9f7dae3f54220a43
rossheat <hi@rossheaton.com>
2021-02-21 12:02:29 +0000

    create example

---
+++ example/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:biokit/biokit.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BioKit Demo',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    print(sayILoveDNA('Ross'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('BioKit'),
+      ),
+    );
+  }
+}
--- lib/biokit.dart
+++ lib/biokit.dart
@@ -1,7 +1,6 @@
 library biokit;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+  sayILoveDNA(String name) {
+    return '$name loves DNA!';
+  }
+
